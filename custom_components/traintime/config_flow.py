3da5216ea7c8757f1b8936b72f05367bd0319aee
Mick-Lab <67667990+Mick-Lab59@users.noreply.github.com>
2025-08-13 23:50:04 +0200

    Update config_flow.py

--- custom_components/traintime/config_flow.py
+++ custom_components/traintime/config_flow.py
@@ -1,8 +1,8 @@
-import voluptuous as vol
 from homeassistant import config_entries
 from .const import DOMAIN
+import voluptuous as vol
 
-class SncfTrainsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class TrainTimeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title=user_input["station_name"], data=user_input)
@@ -16,3 +16,4 @@ class SncfTrainsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             vol.Required("max_trains", default=3): vol.All(int, vol.Range(min=3, max=5))
         })
         return self.async_show_form(step_id="user", data_schema=schema)
+
