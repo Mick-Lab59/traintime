06093faea3b5f75e8509ddce4f527970d3d3d9a3
Mick-Lab <67667990+Mick-Lab59@users.noreply.github.com>
2025-08-14 00:34:39 +0200

    Update __init__.py

--- custom_components/traintime/__init__.py
+++ custom_components/traintime/__init__.py
@@ -4,13 +4,13 @@ from homeassistant.helpers.discovery import async_load_platform
 from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up the SNCF Trains integration from a config entry."""
+    """Set up TrainTime from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    # Charger le platform sensor correctement
-    await async_load_platform(hass, "sensor", DOMAIN, {}, entry)
+    hass.async_create_task(
+        async_load_platform(hass, "sensor", DOMAIN, entry.data, entry)
+    )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload the integration."""
-    # Ici on pourrait supprimer les sensors si nécessaire
     return True
